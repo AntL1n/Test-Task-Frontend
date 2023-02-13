b9fd8b71e0b7a773f5367ff0b3b80813bd54f877
https://github.com/AntL1n <antlin96@gmail.com>
2023-02-13 23:42:03 +0200

    test task by Anton Zdorovets

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,14 @@
-//script
+document.addEventListener("DOMContentLoaded", () => {
+  const modalBtn = document.querySelectorAll(".panel__dropdown-btn");
+
+  modalBtn.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      let currentBtn = e.currentTarget;
+      let drop = currentBtn
+        .closest(".panel__dropdown")
+        .querySelector(".dropdown");
+
+      drop.classList.toggle("dropdown--active");
+    });
+  });
+});
